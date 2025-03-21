c39a91394936bee86f103d12a3353339c7494446
Carlos Daniel <carlos.oficial.ti@gmail.com>
2025-03-21 18:28:13 -0300

    Criação dos modelos de dados (Models)

---
+++ LivrariaAPI/Models/AutorModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace LivrariaAPI.Models {
+    public class AutorModel {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+
+        //Diretiva para que ao criar um Autor, este atributo: Livros, não necessariamente precisa ser preenchido. (Ignora a propriedade).
+        [JsonIgnore]
+        public ICollection<LivroModel> Livros { get; set; }
+    }
+}
