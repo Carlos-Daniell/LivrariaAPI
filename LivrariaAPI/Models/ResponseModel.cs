ac099ec30d6a12599ff6d1cf93943fc13f19f9e5
Carlos Daniel <carlos.oficial.ti@gmail.com>
2025-03-31 23:10:24 -0300

    Modelo de Resposta

--- LivrariaAPI/Models/ResponseModel.cs
+++ LivrariaAPI/Models/ResponseModel.cs
@@ -1,6 +1,7 @@
 ﻿namespace LivrariaAPI.Models {
     public class ResponseModel<T> {
 
+        //Modelo de Resposta
         public T? Dados { get; set; }
         public string Mensagem { get; set; } = string.Empty;
         public bool Status { get; set; } = true;
